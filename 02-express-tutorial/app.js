48f057c8004b3d7095349a62e3f0e04d1bf107c0
camh123 <cameron.h.1732@gmail.com>
2025-07-27 17:23:40 -0500

    finished first part of tutorial

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,33 @@
-console.log('Express Tutorial')
+const express = require('express')
+const app = express()
+let {people} = require('./data')
+
+// static assets
+app.use(express.static('./methods-public'))
+app.use(express.json())
+app.use(express.urlencoded({extended : false}))
+
+
+app.get('/api/people', (req, res) => {
+    res.status(200).json({sucess:true, data:people})
+})
+
+app.post('/api/people', (req, res) => {
+    const{name} = req.body
+    if (!name){
+        return res.status(400).json({sucess:false, msg:'please provide name value'})
+    }
+    res.status(201).json({sucess:true, person : name})
+})
+
+app.post('/login', (req, res) =>{
+    const{name} = req.body;
+    if (name){
+        return res.status(200).send(`Welcone ${name}`)
+    }
+    res.status(401).send('Please send credentials')
+})
+
+app.listen(5000, () => {
+    console.log('Server is listening on port 5000')
+})
